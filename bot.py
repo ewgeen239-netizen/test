2a694291b462028d4ed8c36a30cc9f9c78c9ea12
ewgeen239-netizen <ewgeen239@gmail.com>
2026-06-07 01:55:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 # ── конфиг ────────────────────────────────────────────────────
 TOKEN      = os.environ.get("TOKEN", "8888364212:AAFJcaKMpv2FfxWBx5WJ0nGv8tYWYGYWtBc")
-WEBAPP_URL = os.environ.get("WEBAPP_URL", "https://ewgeen239-netizen.github.io/test/")
+WEBAPP_URL = os.environ.get("WEBAPP_URL", "test-production-ffca.up.railway.app")
 PORT       = int(os.environ.get("PORT", 5000))
 DB_FILE    = "os_data.json"
 RATING_FILE= "rating.json"
